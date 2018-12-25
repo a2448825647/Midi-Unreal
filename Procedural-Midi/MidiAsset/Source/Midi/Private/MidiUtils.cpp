fdab103fb58e1b6b75ecd85332f66037152dadde
Geromatic <scott.bishel@yahoo.com>
2018-12-25 04:11:55 -0800

    ...

--- Procedural-Midi/MidiAsset/Source/Midi/Private/MidiUtils.cpp
+++ Procedural-Midi/MidiAsset/Source/Midi/Private/MidiUtils.cpp
@@ -3,9 +3,8 @@
 #include "MidiUtils.h"
 #include "MidiPrivatePCH.h"
 
-#include <math.h>
-
-static const float MIN_FREQUENCY = (440.0f / 32.0f); // MIDI Note Number 9
+static const float CENTER_FREQUENCY = 440.0f;
+static const float MIN_FREQUENCY = (CENTER_FREQUENCY / 32.0f); // MIDI Note Number 9
 
 float UMidiUtils::DefaultNoteToFrequency(TEnumAsByte<ENoteEnum> note) {
 	return NoteToFrequency((uint8)note + 60);
@@ -14,37 +13,37 @@ float UMidiUtils::DefaultNoteToFrequency(TEnumAsByte<ENoteEnum> note) {
 float UMidiUtils::NoteToFrequency(uint8 note) {
 	//	Hertz = 440.0 * pow(2.0, (midi note - 69) / 12);
 	// Diatonic Scale
-	float Frequency = MIN_FREQUENCY * powf(2.0f, (note - 9) / 12);
+	float Frequency = MIN_FREQUENCY * FMath::Pow(2.0, (note - 9) / 12.0);
 	return Frequency;
 }
 
 uint8 UMidiUtils::FrequencyToNote(float Frequency) {
 	// midi note = log(Hertz/440.0)/log(2) * 12 + 69;
-	int32 MidiNote = logf(Frequency / MIN_FREQUENCY ) / logf(2) * 12 + 9;
+	int32 MidiNote = 9 + 12 * FMath::Log2(Frequency / MIN_FREQUENCY);
 	return MidiNote;
 }
 
 float UMidiUtils::SemitoneToFrequency(int32 semitone) {
 	// frequency = 2^(semitone/12) * 440 Hz
-	float Frequency = powf(2.0f, semitone / 12) * MIN_FREQUENCY;
+	float Frequency = FMath::Pow(2.0, semitone / 12.0) * MIN_FREQUENCY;
 	return Frequency;
 }
 
 int32 UMidiUtils::FrequencyToSemitone(float Frequency) {
-	// semi tone = 12*(log (Frequency/440) / log(2)) 
-	int32 Semitone = logf(Frequency / MIN_FREQUENCY) / logf(2.0f) * 12;
+	// n = 12*(log (Frequency/440) / log(2)) 
+	int32 Semitone = 12 * FMath::Log2(Frequency / MIN_FREQUENCY);
 	return Semitone;
 }
 
 int32 UMidiUtils::FrequencyToCent(float Frequency) {
-	// cent = log(Hertz/440.0)/log(2) * 1200;
-	int32 Cent = logf(Frequency / MIN_FREQUENCY) / logf(2.0f) * 1200;
+	// cent = log(Hertz/440.0)/log(2) * 12;
+	int32 Cent = FMath::Log2(Frequency / MIN_FREQUENCY) * 1200.0;
 	return Cent;
 }
 
 int32 UMidiUtils::FrequencyToOctave(float Frequency) {
 	// octave = log(Hertz/440.0)/log(2);
-	int32 Octave = logf(Frequency / MIN_FREQUENCY) / logf(2.0f);
+	int32 Octave = FMath::Log2(Frequency / MIN_FREQUENCY);
 	return Octave;
 }
 
